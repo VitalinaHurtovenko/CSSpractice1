4d5aa75fe4a76e0cdb99416d50dadb9acce53789
Vitalina Hurtovenko <vitalina.hurtovenko.18@gmail.com>
2023-10-25 15:43:48 +0300

    lesson 8 css

--- src/route/index.js
+++ src/route/index.js
@@ -3,20 +3,22 @@ const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
 
-// ================================================================
 
-// router.get Створює нам один ентпоїнт
 
-//           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
   res.render('index', {})
   //                  ↑↑ сюди вводимо JSON дані
 })
 
-// ================================================================
+router.get('/selector', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  res.render('selector', {
+    layout: "selector",
+  })
+  
+  //                  ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
